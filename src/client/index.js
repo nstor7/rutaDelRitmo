e1762c74bf7ef11ba90a304ae7e6c9ee9d86c32a
Néstor Ibarra <nstor7@hotmail.com>
2015-12-09 21:28:14 -0500

    Layout del sitio

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,31 @@
-import angular from 'angular'
+import $ from 'jquery'
+
+$(function(){
+
+    var template = '<article>' +
+            '<img src=":img:" alt=":imgAlt:">' +
+        '<div>' +
+                '<h1>:name:</h1>' +
+                '<p>:description:</p>' +
+            '</div>' +
+        '</article>'
+    $.ajax({
+        url: 'http://api.tvmaze.com/shows',
+        success: function(shows, textStatus, xhr){
+            var $tvShowsContainter = $('#app-container').find('#juego')
+            $tvShowsContainter.find('.loader').remove()
+            shows.forEach(function(show){
+                var article = template
+                    .replace(':name:', show.name)
+                    .replace(':img:', show.image.medium)
+                    .replace(':description:', show.summary)
+                    .replace(':imgAlt:', show.name + ' logo')
+                
+                    $tvShowsContainter.append($(article))
+            })
+        }
+    })
+    
+})
+
+    
